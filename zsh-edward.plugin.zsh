1c69cc3e9857fe7ab92b54db3c823f55762a6f8a
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2019-12-28 20:09:15 +0100

    feat: added aliases and documentation

--- zsh-edward.plugin.zsh
+++ zsh-edward.plugin.zsh
@@ -2,6 +2,13 @@
 ##### ALIASES
 ########################################################
 
-# TODO Add aliases
-
-# TODO see if things can be added here
+alias edgen='edward generate'
+alias edh='edward help'
+alias edlist='edward list'
+alias edrestart='edward restart'
+alias edstart='edward start'
+alias edstatus='edward status'
+alias edstop='edward stop'
+alias edtail='edward tail'
+alias edtiplog='edward tiplog'
+alias edv='edward version'
